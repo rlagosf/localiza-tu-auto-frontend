17f563ccf769d0fec446722efe87c790aa42f1ba
rlagosf <rodrigo.lagos.fernandez@gmail.com>
2026-03-03 11:41:23 -0300

    cambios finales para deploy 03032026 1141

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ producción en raíz
+  base: "/localiza-tu-auto-frontend/", // ✅ producción en raíz
 });
